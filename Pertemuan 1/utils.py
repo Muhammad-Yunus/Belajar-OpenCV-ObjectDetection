c8df01e8ae6c4d5b3e84a9f8b5c623ec0932746d
Muhammad Yunus <muhammadyunusiot@gmail.com>
2024-08-28 13:40:13 +0700

    update pertemuan 1

---
+++ Pertemuan 1/utils.py
@@ -0,0 +1,24 @@
+import cv2
+
+def draw_ped(img, label, x0, y0, xt, yt, color=(255,127,0), text_color=(255,255,255)):
+
+    (w, h), baseline = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+    cv2.rectangle(img,
+                  (x0, y0 + baseline),  
+                  (max(xt, x0 + w), yt), 
+                  color, 
+                  2)
+    cv2.rectangle(img,
+                  (x0, y0 - h),  
+                  (x0 + w, y0 + baseline), 
+                  color, 
+                  -1)  
+    cv2.putText(img, 
+                label, 
+                (x0, y0),                   
+                cv2.FONT_HERSHEY_SIMPLEX,     
+                0.5,                          
+                text_color,                
+                1,
+                cv2.LINE_AA) 
+    return img
